31bd898fb6ffe546c172df4ed55541a0544d94b9
hans <Hans@fanruan.com>
2023-07-24 11:55:55 +0800

    feat(ts/eslint config): ts/eslint base config

--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,46 @@
-const { join } = require("path");
+const path = require('path')
+const ESLintPlugin = require('eslint-webpack-plugin')
 
 module.exports = {
-    entry: "./src/index",
+    entry: './src/index',
+    mode: 'development',
     output: {
-        filename: "[name].js",
-        path: join(__dirname, "./dist"),
+        filename: '[name].js',
+        path: path.resolve(__dirname, 'dist'),
     },
+    module: {
+        rules: [
+            {
+                test: /\.less$/i,
+                use: [
+                    'style-loader',
+                    'css-loader',
+                    {
+                        loader: 'less-loader',
+                    },
+                ],
+            },
+            {
+                test: /\.js$/,
+                use: [
+                    {
+                        loader: 'babel-loader',
+                        options: {
+                            presets: ['@babel/preset-env'],
+                        },
+                    },
+                ],
+            },
+            {
+                test: /\.ts$/,
+                use: {
+                    loader: 'babel-loader',
+                    options: {
+                        presets: ['@babel/preset-typescript'],
+                    },
+                },
+            },
+        ],
+    },
+    'plugins': [new ESLintPlugin({ extensions: ['.js', '.ts'] })],
 }
