fea0fd03ec3c0356da290d481eb5067940aa9ca8
hans <Hans@fanruan.com>
2023-07-31 16:00:38 +0800

    feat: q

--- src/index.tsx
+++ src/index.tsx
@@ -1,8 +1,5 @@
-import { createRoot } from 'react-dom/client'
+import {createRoot} from 'react-dom/client';
+import MainLayout from './layout/MainLayout';
 
-// 清除现有的 HTML 内容
-document.body.innerHTML = '<div id="app"></div>'
-
-// 渲染你的 React 组件
-const root = createRoot(document.getElementById('app'))
-root.render(<h1>Hello, world</h1>)
+const root = createRoot(document.getElementById('root'));
+root.render(<MainLayout/>);
