38ef071dfa3618f4220f3565e93fb54bc4743508
hans <Hans@fanruan.com>
2024-01-06 19:06:38 +0800

    现代web布局学习 fix for eslint & simple demo

--- src/example/web-layout/Layout.tsx
+++ src/example/web-layout/Layout.tsx
@@ -33,32 +33,38 @@ export const Layout = () => {
   };
 
   return (
-    <div className="layout">
-      {new Array(WidgetExampleSize).fill(0, 0, WidgetExampleSize).map((v, index) => {
-        return (
-          <div className="widget-container">
-            {InputComponent(index)}
-            <div className="exhibition" style={style[index]} children={1} />
-          </div>
-        );
-      })}
-      {new Array(LayoutExampleSize).fill(0, 0, WidgetExampleSize).map((v, index) => {
-        const realIndex = index + WidgetExampleSize;
-        return (
-          <div className="layout-container" style={style[realIndex]}>
-            {InputComponent(realIndex)}
-            <div className="exhibition" children={1} />
-            <div className="exhibition" children={2} />
-            <div className="exhibition" children={3} />
-            <div className="exhibition" children={4} />
-            <div className="exhibition" children={5} />
-            <div className="exhibition" children={6} />
-            <div className="exhibition" children={7} />
-            <div className="exhibition" children={8} />
-            <div className="exhibition" children={9} />
-          </div>
-        );
-      })}
-    </div>
+    <>
+      <div>111</div>
+      <div>222</div>
+      <span>111</span>
+      <span>222</span>
+      <div className="layout">
+        {new Array(WidgetExampleSize).fill(0, 0, WidgetExampleSize).map((v, index) => {
+          return (
+            <div className="widget-container">
+              {InputComponent(index)}
+              <div className="exhibition" style={style[index]} children={1} />
+            </div>
+          );
+        })}
+        {new Array(LayoutExampleSize).fill(0, 0, WidgetExampleSize).map((v, index) => {
+          const realIndex = index + WidgetExampleSize;
+          return (
+            <div className="layout-container" style={style[realIndex]}>
+              {InputComponent(realIndex)}
+              <div className="exhibition" children={11111111111111} />
+              <div className="exhibition" children={2} />
+              <div className="exhibition" children={3} />
+              <div className="exhibition" children={4} />
+              <div className="exhibition" children={5} />
+              <div className="exhibition" children={6} />
+              <div className="exhibition" children={7} />
+              <div className="exhibition" children={8} />
+              <div className="exhibition" children={9} />
+            </div>
+          );
+        })}
+      </div>
+    </>
   );
 };
