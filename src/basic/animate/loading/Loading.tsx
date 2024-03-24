e5b61827a6fd5df67452450aa0a4e13c5dfbc680
hans <Hans@fanruan.com>
2024-03-24 18:10:17 +0800

    animate Instance -- loading

--- src/basic/animate/loading/Loading.tsx
+++ src/basic/animate/loading/Loading.tsx
@@ -1,3 +1,12 @@
+import { range } from "lodash";
+import "./Loading.less";
+
 export const Loading = () => {
-  return <div></div>;
+  return(
+    <div className="g-container g-steps">
+      {range(0,20,1).map(
+        (value, index, array) => <div className="g-box" style={{ "--index": index }} />
+      )}
+    </div>
+  );
 };
--- src/index.tsx
+++ src/index.tsx
@@ -1,12 +1,14 @@
 import { createRoot } from "react-dom/client";
+import "./index.less";
 
 // import { Instance } from "./basic/css/instance/Instance";
 
 // import MainLayout from "./layout/MainLayout";
 // import { Animate } from "./basic/animate/spring/Animate";
 import Marquee from "./basic/animate/marquee/Marquee";
+import { Loading } from "./basic/animate/loading/Loading";
 
 const root = createRoot(document.getElementById("root"));
 root.render(
-  <Marquee text={"故大德必得其位，必得其禄，必得其名，必得其寿。故天生之物，必因其材而笃焉."} />,
+  <Loading />,
 );
