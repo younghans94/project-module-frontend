727fc316e4f854938e8e65f4d40388615aa4c217
Hans <Hans@fanruan.com>
2024-03-27 19:59:44 +0800

    animation feature

---
+++ src/basic/animate/feature/Feature.tsx
@@ -0,0 +1,11 @@
+import "./Feature.less";
+
+export const Feature = () => {
+  return (
+    <div className="container">
+      <div className="mix add"></div>
+      <div className="mix accumulate"></div>
+      <div className="mix replace"></div>
+    </div>
+  );
+};
